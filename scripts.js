3f07e59d2497940a555cec8e252f12b6b9705efd
jGibao <joao.gibao@techremotehub.com>
2022-03-22 13:39:31 +0000

    update interface

--- scripts.js
+++ scripts.js
@@ -2,107 +2,97 @@ let pedra = "pedra";
 let papel = "papel";
 let tesoura = "tesoura";
 let rondasJogadas = 0;
-let pontos = 0;
+let pontosJog = 0;
+let pontosComp = 0;
 var imagemJogador;
 var imagemComputador;
-var textJogadores;
+var textJog;
+var textComp;
+var botoes;
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function prep() {
-    imagemJogador = document.getElementById("jogador");
-    imagemComputador = document.getElementById("computador");
-    textJogadores = document.getElementById("jogadores");
+    imagemJogador = document.getElementById("jogadorImg");
+    imagemComputador = document.getElementById("computadorImg");
+    textJog = document.getElementById("jogador");
+    textComp = document.getElementById("computador");
+    botoes = document.getElementsByClassName("button");
     imagemJogador.style.opacity = "100%";
     imagemComputador.style.opacity = "100%";
     imagemComputador.style.transform = "scaleX(-1)";
-    resultado.style.opacity = "100%";
 }
 
 function jogar(selecao) {
     prep();
-    textJogadores.style.opacity = "100%";
     rondasJogadas++;
     let computador = randomIntFromInterval(1, 3);
-    var resultado = document.getElementById("resultado");
-    resultado.classList.remove("amarelo");
-    resultado.classList.remove("vermelho");
-    resultado.classList.remove("verde");
     var final = document.getElementById("final");
     final.style.color = 'white';
     switch (computador) {
         case 1: //pedra
             if (selecao == pedra) {
-                resultado.textContent = "Escolheste pedra. O computador também escolheu pedra. 0 Pontos.";
-                resultado.classList.add("amarelo");
                 imagemJogador.src = "pedra.png";
                 imagemComputador.src = "pedra.png";
 
             } else if (selecao == papel) {
-                resultado.textContent = "Escolheste papel. O computador escolheu pedra. +1 Ponto!";
-                pontos++;
-                resultado.classList.add("verde");
+                pontosJog++;
                 imagemJogador.src = "papel.png";
                 imagemComputador.src = "pedra.png";
             } else if (selecao == tesoura) {
-                resultado.textContent = "Escolheste tesoura. O computador escolheu pedra. -1 Ponto!";
-                pontos--;
-                resultado.classList.add("vermelho");
+                pontosComp++;
                 imagemJogador.src = "tesoura.png";
                 imagemComputador.src = "pedra.png";
             }
             break;
         case 2: //papel
             if (selecao == pedra) {
-                resultado.textContent = "Escolheste pedra. O computador escolheu papel. -1 Ponto!";
-                pontos--;
-                resultado.classList.add("vermelho");
+                pontosComp++;
                 imagemJogador.src = "pedra.png";
                 imagemComputador.src = "papel.png";
             } else if (selecao == papel) {
-                resultado.textContent = "Escolheste papel. O computador também escolheu papel. 0 Pontos.";
-                resultado.classList.add("amarelo");
                 imagemJogador.src = "papel.png";
                 imagemComputador.src = "papel.png";
             } else if (selecao == tesoura) {
-                resultado.textContent = "Escolheste tesoura. O computador escolheu papel. +1 Ponto!";
-                pontos++;
-                resultado.classList.add("verde");
+                pontosJog++;
                 imagemJogador.src = "tesoura.png";
                 imagemComputador.src = "papel.png";
             }
             break;
         case 3: // tesoura
             if (selecao == pedra) {
-                resultado.textContent = "Escolheste pedra. O computador escolheu tesoura. +1 Ponto!";
-                pontos++;
-                resultado.classList.add("verde");
+                pontosJog++;
                 imagemJogador.src = "pedra.png";
                 imagemComputador.src = "tesoura.png";
             } else if (selecao == papel) {
-                resultado.textContent = "Escolheste papel. O computador escolheu tesoura. -1 Ponto!";
-                pontos--;
-                resultado.classList.add("vermelho");
+                pontosComp++;
                 imagemJogador.src = "papel.png";
                 imagemComputador.src = "tesoura.png";
             } else if (selecao == tesoura) {
-                resultado.textContent = "Escolheste tesoura. O computador também escolheu tesoura. 0 Pontos.";
-                resultado.classList.add("amarelo");
                 imagemJogador.src = "tesoura.png";
                 imagemComputador.src = "tesoura.png";
             }
             break;
 
     }
-
+    for (let botao of botoes) {
+        botao.disabled = true;
+    }
     if (rondasJogadas < 5) {
+        textJog.textContent = "Jogador: " + pontosJog;
+        textComp.textContent = "Computador: " + pontosComp;
         setTimeout(() => {
             imagemJogador.style.opacity = "0%";
             imagemComputador.style.opacity = "0%";
-            textJogadores.style.opacity = "0%";
+
         }, 2000);
+        setTimeout(() => {
+            for (let botao of botoes) {
+                botao.disabled = false;
+            }
+        }, 2500);
     }
     if (rondasJogadas == 5) {
         acabaJogo();
@@ -115,14 +105,20 @@ function acabaJogo() {
         resultado.style.opacity = "0%";
     }, 1500);
     final.style.color = 'black';
-    if (pontos > 0) {
-        final.textContent = "Ganhaste o jogo com " + pontos + " ponto(s)!";
-    } else if (pontos == 0) {
-        final.textContent = "Empate!";
-    } else if (pontos < 0) {
-        final.textContent = "Perdeste o jogo!";
+    if (pontosJog > pontosComp) {
+        final.textContent = "Ganhaste o jogo por " + (pontosJog - pontosComp) + " ponto(s)!";
+    } else if (pontosJog == pontosComp) {
+        final.textContent = "Empate com " + pontosJog + " pontos.";
+    } else if (pontosJog < pontosComp) {
+        final.textContent = "Perdeste o jogo por " + (pontosComp - pontosJog) + " ponto(s).";
     }
     rondasJogadas = 0;
-    pontos = 0;
+    pontosJog = 0;
+    pontosComp = 0;
+    setTimeout(() => {
+        for (let botao of botoes) {
+            botao.disabled = false;
+        }
+    }, 2500);
     //window.location.reload();
 }
